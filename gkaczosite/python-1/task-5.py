2ccd622f679b233d1cdb70f1a9998ded0c9cdec4
Muffin <muffin.time231@gmail.com>
2023-10-05 17:37:01 +0200

    finish of task-5.py

--- gkaczosite/python-1/task-5.py
+++ gkaczosite/python-1/task-5.py
@@ -1,8 +1,68 @@
+import re
+
+
 def stringToUpperCase(text):
     return text.upper()
+
 
 def firstLetterToUpperCase(text):
     textInList = list(text)
 
+    i = 0
     for letter in textInList:
-        if letter == 
+        if not re.search("\W", letter):
+            textInList[i] = letter.upper()
+            break
+
+        i += 1
+
+    return ''.join(textInList)
+
+
+def searchForLook(text):
+    found = re.search('(look)', text)
+
+    if not found:
+        return 'No "look" in text'
+
+    position = found.span()[0]
+
+    return position
+
+
+def checkIfAnythingOtherThanDigitsOrLetterExist(text):
+    check = re.search("\W", text)
+    if check:
+        return 'True'
+    else:
+        return 'False'
+
+
+def deleteWhitespaces(text):
+    textWithoutWhitespaces = text.replace(' ', '')
+
+    return textWithoutWhitespaces
+
+
+def seperateWords(text):
+    seperateWords = re.split("\s", text)
+
+    return seperateWords
+
+
+def eraseFirstAndLastIndex(text):
+    textList = list(text)
+
+    textList.pop()
+    textList.pop(0)
+
+    return ''.join(textList)
+
+
+def iDontEvenKnowAnymore(text):
+    text = text.replace("Always look on the bright side of life", "Always look on the bright side of your life")
+
+    return text
+
+
+text = "Always look on the bright side of life"
